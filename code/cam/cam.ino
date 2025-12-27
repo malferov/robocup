940771c204870778497395c6c5d6ac79ac7defa9
Maxim Alferov <malferov44@gmail.com>
2025-12-27 10:56:19 +0100

    cam capture

--- code/cam/cam.ino
+++ code/cam/cam.ino
@@ -1,19 +1,5 @@
 #include "esp_camera.h"
-#include <WiFi.h>
 
-// ===========================
-// Select camera model in board_config.h
-// ===========================
-#include "board_config.h"
-
-// ===========================
-// Enter your WiFi credentials
-// ===========================
-/*
-const char *ssid = "";
-const char *password = "";
-void startCameraServer();
-*/
 typedef struct {
   int r, g, b;
 } color_t;
@@ -23,122 +9,22 @@ typedef struct {
   int val;
 } position_t;
 
-void setupLedFlash();
+const int width = 96;
+const int horizon = 48;
+const int threshold = 8;
+const color_t no_color = color_t(0,0,0); //black
+const position_t zero_position = position_t('L', 0);
+const int buttonPin = 13;
+
+color_t g_goal_color = no_color;
+
+void camSetup();
 
 void setup() {
   Serial.begin(9600);
-  //Serial.begin(115200);
-  //Serial.setDebugOutput(true);
   Serial.println();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_96X96;
-  config.pixel_format = PIXFORMAT_RGB565;
-  //config.pixel_format = PIXFORMAT_RGB888;
-  //config.pixel_format = PIXFORMAT_YUV422;
-  //config.pixel_format = PIXFORMAT_GRAYSCALE;
-  //config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-  //config.jpeg_quality = 1;
-  //config.pixel_format = PIXFORMAT_RAW;
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  //config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.fb_location = CAMERA_FB_IN_DRAM;
-  config.fb_count = 1;
-/*
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
-*/
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-  sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
-
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  setupLedFlash();
-#endif
-/*
-  WiFi.begin(ssid, password);
-  WiFi.setSleep(false);
-
-  Serial.print("WiFi connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  startCameraServer();
-  Serial.println("Camera Ready");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-*/
+  camSetup();
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 color_t get_color(uint8_t * buf, int offset){
@@ -150,25 +36,51 @@ color_t get_color(uint8_t * buf, int offset){
   return color_t(r, g, b);
 }
 
-int width = 96;
-int horizon = 48;
+bool is_color(color_t sample, color_t color){
+  return (abs(sample.r-color.r) < threshold && abs(sample.g-color.g) < threshold && abs(sample.b-color.b) < threshold);
+}
 
-position_t get_goal_position(uint8_t * buf, color_t goal_color){
+color_t capture_color(){
+  //capture a frame
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    ESP_LOGE(TAG, "Frame buffer could not be acquired");
+    return no_color; //black
+  }
+  //color_t yellow = color_t(31, 63, 28);
+  int center = width * 2*48 + 2*48; // center pixel (9312)
+  color_t color = get_color(fb -> buf, center);
+  //return the frame buffer back to be reused
+  esp_camera_fb_return(fb);
+
+  // debug
+  //int bytes = (fb->buf[9312]<<8) + fb->buf[9312+1];
+  //Serial.printf("rgb %d %d %d color %s bytes %04X\n", color.r, color.g, color.b, color_s, bytes);
+  return color;
+}
+
+position_t get_goal_position(color_t goal_color){
+  //capture a frame
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    ESP_LOGE(TAG, "Frame buffer could not be acquired");
+    return zero_position;
+  }
   int offset = horizon * width * 2;
   int left=0;
   int right=0;
   for (int i=0; i<width; i++) {
-    color_t color = get_color(buf, offset + i*2);
+    color_t color = get_color(fb -> buf, offset + i*2);
     if (left == 0 && is_color(color, goal_color)){
       left = i;
     }
-    if (left > 0 && right == 0 && !is_color(color, goal_color)){
-      right = i;
-    }
-    if (right > 0 && is_color(color, goal_color)){
+    if (is_color(color, goal_color)){
       right = i;
     }
   }
+  //return the frame buffer back to be reused
+  esp_camera_fb_return(fb);
+
   int center = (right + left)/2;
   position_t position;
   if (center > width/2) {
@@ -177,49 +89,26 @@ position_t get_goal_position(uint8_t * buf, color_t goal_color){
     position.dir = 'L';
   }
   position.val = 8 * abs(center - width/2) / (width/2);
+  if (left == 0 or right == 0){
+    position = zero_position;
+  }
   // debug
-  //Serial.printf("left %d right %d center %d position %c%d\n", left, right, center, position.dir, position.val);
-  //Serial.printf("center %d position %c%d\n", center, position.dir, position.val);
+  Serial.printf("left %d right %d center %d goal_color %d %d %d ", left, right, center, goal_color.r, goal_color.g, goal_color.b);
+  Serial.printf("position %c%d\n", position.dir, position.val);
   return position;
 }
 
-int threshold = 8;
-
-bool is_color(color_t sample, color_t color){
-  return (abs(sample.r-color.r) < threshold && abs(sample.g-color.g) < threshold && abs(sample.b-color.b) < threshold);
-}
-esp_err_t camera_capture(){
-        //capture a frame
-        camera_fb_t * fb = esp_camera_fb_get();
-        if (!fb) {
-            ESP_LOGE(TAG, "Frame buffer could not be acquired");
-            return ESP_FAIL;
-        }
-
-        String color_s;
-        color_t black = color_t(5, 11, 6);
-        color_t yellow = color_t(31, 63, 28);
-        color_t blue = color_t(8, 28, 21);
-        int center = width * 2*48 + 2*48; // center pixel (9312)
-        color_t color = get_color(fb -> buf, center);
-
-        if (is_color(color, black)) {
-          color_s = "black";
-        } else if (is_color(color, yellow)) {
-          color_s = "yellow";
-        } else if (is_color(color, blue)) {
-          color_s = "blue";
-        }
-        // debug
-        //int bytes = (fb->buf[9312]<<8) + fb->buf[9312+1];
-        //Serial.printf("rgb %d %d %d color %s bytes %04X\n", color.r, color.g, color.b, color_s, bytes);
-        position_t position = get_goal_position(fb -> buf, yellow);
-        
-        //return the frame buffer back to be reused
-        esp_camera_fb_return(fb);
-        Serial.printf("%c:%d\n",position.dir,position.val);
-        //Serial.printf("test \n");
-        return ESP_OK;
+bool buttonPressed(){
+  // check if pressed for at least 100 ms
+  bool pressed = true;
+  for (int i=0; i<10; i++) {
+    int state = digitalRead(buttonPin);
+    if (state == HIGH){
+      pressed = false;
+    }
+    delay(10);
+  }
+  return pressed;
 }
 
 void loop() {
@@ -227,9 +116,17 @@ void loop() {
     String received = Serial.readStringUntil('\n');
     received.trim();
     if (received == "get_pos") {
-      esp_err_t err = camera_capture();
+      position_t position = get_goal_position(g_goal_color);
+      Serial.printf("%c:%d\n", position.dir, position.val);
     }
   }
-  //Serial.printf("test main loop\n");
-  //delay(100);
+  if (buttonPressed()) {
+    g_goal_color = capture_color();
+    //debug
+    //Serial.printf("button pressed; goal color %d %d %d\n", g_goal_color.r, g_goal_color.g, g_goal_color.b);
+  }
+  //debug
+  //position_t position = get_goal_position(g_goal_color);
+  //Serial.printf("%c:%d\n", position.dir, position.val);
+  //delay(200);
 }
