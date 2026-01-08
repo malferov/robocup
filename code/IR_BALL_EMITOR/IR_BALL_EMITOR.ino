33be32a3f91dc74d8c00065a67fc7df3e0d24a2c
onestep330 <2000sa34@gmail.com>
2026-01-08 21:37:08 +0100

    Create IR_BALL_EMITOR.ino

---
+++ code/IR_BALL_EMITOR/IR_BALL_EMITOR.ino
@@ -0,0 +1,16 @@
+#define PWM_PIN 13
+
+void setup() {
+  // Set PWM frequency
+  analogWriteFrequency(PWM_PIN, 40000);  // 40 kHz
+
+  // Set PWM resolution (bits)
+  analogWriteResolution(PWM_PIN, 8);     // 0–255
+
+  // 50% duty cycle
+  analogWrite(PWM_PIN, 128);
+}
+
+void loop() {
+  // PWM runs in hardware
+}
